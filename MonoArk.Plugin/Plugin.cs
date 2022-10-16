f8c611506e76bdf25c738c0c200b4183c229be0b
frostu8 <frostu8@protonmail.com>
2022-10-16 08:43:41 -0400

    setup initial project

---
+++ MonoArk.Plugin/Plugin.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+
+namespace MonoArk.Plugin
+{
+    [BepInPlugin(Plugin.GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInProcess("ChronoArk.exe")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string GUID = "net.frostu8.chronoark.monoark";
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+    }
+}
+
